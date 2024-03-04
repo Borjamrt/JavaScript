03a2785ce8cde4966f7a647454df5b355f1bed5f
Borjamrt <borjamrt@outlook.com>
2024-03-04 10:33:35 +0100

    Ejercicio

--- src/main.js
+++ src/main.js
@@ -1,23 +1,6 @@
-import "./style.css";
-
-console.log("Hola Mundo");
-function areaDelCuadrado(lado){
-    return lado*lado
-}
-console.log(areaDelCuadrado(4));
-console.log(areaDelCuadrado(8));
-function areaDelRectangulo(base,altura){
-    return base*altura
-}
-console.log(areaDelRectangulo(4,3));
-
-function sumar(){
-    const sumando1 = document.getElementById("sumando1").value;
-    const sumando2 = document.getElementById("sumando2").value;
-    const resultado = parseInt(sumando1) + parseInt(sumando2);
-    document.getElementById("resultado").innerHTML = resultado;
-    const botonSumar = document.getElementById("sumar");
-    botonSumar.addEventListener("click", sumar);
-    print resultado
-}
-
+<script>
+    let precio = 200;
+    let iva = 21;
+    let precioTotal= precio + precio*21/100
+    console.log("El precio total es:" +precioTotal);
+</script>
